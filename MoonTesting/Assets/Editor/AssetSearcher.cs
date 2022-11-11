afb2d9bf2b5d43b69cd0de433cf7e9f4e0f96e91
gregtom6 <gregtom6@gmail.com>
2022-11-11 13:21:40 +0100

    listing fields and toggles near them

--- MoonTesting/Assets/Editor/AssetSearcher.cs
+++ MoonTesting/Assets/Editor/AssetSearcher.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 
 public class AssetSearcher : EditorWindow
 {
     UnityEngine.Object newObject = null;
-
+    Dictionary<FieldInfo, ShouldISearchForItAndString> fieldsAndTheirValuesToSearchFor = new Dictionary<FieldInfo, ShouldISearchForItAndString>();
+    List<string> valueOfFields = new List<string>();
+    List<bool> shouldISearchForBools = new List<bool>();
+    System.Type previousType;
 
     [MenuItem("MoonStudios/AssetSearcher")]
     public static void ShowWindow()
@@ -18,8 +22,48 @@ public class AssetSearcher : EditorWindow
     {
         newObject = EditorGUILayout.ObjectField("", newObject, typeof(UnityEngine.Object), true);
 
+        if (newObject is MonoScript)
+        {
+            MonoScript monoScript = (MonoScript)newObject;
+            System.Type monoBehaviourType = monoScript.GetClass();
 
-        
+            if (monoBehaviourType != previousType)
+            {
+                valueOfFields.Clear();
+                shouldISearchForBools.Clear();
+                fieldsAndTheirValuesToSearchFor.Clear();
+            }
+
+            for (int i = 0; i < monoBehaviourType.GetFields().Length; i++)
+            {
+                valueOfFields.Add("");
+                shouldISearchForBools.Add(false);
+                if (!fieldsAndTheirValuesToSearchFor.ContainsKey(monoBehaviourType.GetFields()[i]))
+                    fieldsAndTheirValuesToSearchFor.Add(monoBehaviourType.GetFields()[i], new ShouldISearchForItAndString()
+                    {
+                        shouldISearchForIt = false,
+                        value = "",
+                    });
+            }
+
+            for (int i = 0; i < monoBehaviourType.GetFields().Length; i++)
+            {
+                FieldInfo field = monoBehaviourType.GetFields()[i];
+                GUILayout.BeginHorizontal();
+                shouldISearchForBools[i] = EditorGUILayout.Toggle("search for it? ", shouldISearchForBools[i]);
+                valueOfFields[i] = EditorGUILayout.TextField(field.Name + " " + field.FieldType.ToString() + " ", valueOfFields[i]);
+                fieldsAndTheirValuesToSearchFor[field] = new ShouldISearchForItAndString()
+                {
+                    shouldISearchForIt = shouldISearchForBools[i],
+                    value = valueOfFields[i],
+                };
+                GUILayout.EndHorizontal();
+            }
+
+
+            previousType = monoBehaviourType;
+        }
+
         if (GUILayout.Button("search"))
         {
             Search();
@@ -30,4 +74,10 @@ public class AssetSearcher : EditorWindow
     {
 
     }
+}
+
+public class ShouldISearchForItAndString
+{
+    public bool shouldISearchForIt;
+    public object value;
 }
